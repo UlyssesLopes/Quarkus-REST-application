8fa545644b1c5fe8e9b1966a68853aa0e5c415ee
Ulysses Lopes <ulysses.rlopes@gmail.com>
2023-02-17 15:23:56 -0300

    Commit application using Quarkus, Panache, Postgres and Hibernate.

---
+++ src/main/java/org/acme/repository/ProductRepository.java
@@ -0,0 +1,14 @@
+package org.acme.repository;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import org.acme.entity.ProductEntity;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class ProductRepository implements PanacheRepository<ProductEntity> {
+
+
+
+}
